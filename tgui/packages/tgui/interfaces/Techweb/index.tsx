ecf9f9df1f1328691c8f51ba04eeb1bbb8b691f6
ZealousZeke <85478781+ZealousZeke@users.noreply.github.com>
2026-07-22 23:45:42 -0500

    readd scroll bar to research

--- tgui/packages/tgui/interfaces/Techweb/index.tsx
+++ tgui/packages/tgui/interfaces/Techweb/index.tsx
@@ -4,7 +4,7 @@ import { TechwebStart } from './Start';
 export function Techweb(props) {
   return (
     <Window width={640} height={735}>
-      <Window.Content>
+      <Window.Content scrollable>
         <TechwebStart />
       </Window.Content>
     </Window>
@@ -14,7 +14,7 @@ export function Techweb(props) {
 export function AppTechweb(props) {
   return (
     <NtosWindow width={640} height={735}>
-      <NtosWindow.Content>
+      <NtosWindow.Content scrollable>
         <TechwebStart />
       </NtosWindow.Content>
     </NtosWindow>
